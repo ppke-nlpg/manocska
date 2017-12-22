ae08a7da35553e8c7db17ad2bc51c48740301b32
Balázs Indig <indba@digitus.itk.ppke.hu>
2017-12-22 19:39:28 +0100

    Fix statistics computing script

--- stat.sh
+++ stat.sh
@@ -3,9 +3,9 @@ cat manocska.txt | wc -l
 echo -n "all INF frames: "
 cat manocska.txt  | grep "INF" -c
 echo -n "INF frames (in ONE resource): "
-cat manocska.txt | grep "INF" | cut -d$'\t' -f5,7 | egrep $'0\t|\t0' -c
+cat manocska.txt | grep "INF" | cut -d$'\t' -f5,7 | grep -v $'^[1-9][0-9]*\t[1-9][0-9]*$' -c
 echo -n "INF frames (in TWO resource): "
-cat manocska.txt | grep "INF" | cut -d$'\t' -f5,7 | egrep -v $'0\t|\t0' -c
+cat manocska.txt | grep "INF" | cut -d$'\t' -f5,7 | grep $'^[1-9][0-9]*\t[1-9][0-9]*$' -c
 echo -n "all NON-INF frames: "
 cat manocska.txt | grep -v "INF" -c
 echo -n "NON-INF frames (in ONE resources): "
